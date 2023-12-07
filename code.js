b3c01ae1f61dfbb842fcadc59a9334364d4ad9b4
wyo-kaitlyn <wyokaitlyn@gmail.com>
2023-12-07 11:44:07 -0700

    dijkstra's

--- code.js
+++ code.js
@@ -1,3 +1,49 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    // check for empty graph
+    if(graph.length < 1) {
+        return [];
+    }
+
+    let dist = [];
+    let visited = [];
+    // intializing dist to Infinty
+    // and visited to false
+    for (node in graph) {
+        dist[node] = Infinity;
+        visited[node] = false;
+    }
+    // source to 0
+    dist[sourceNode] = 0;
+
+    for (i in visited) {
+        let min = Infinity;
+        let index = -1;
+        let temp = 0;
+
+        // loop through the positions in the adjacency matrix to find
+        // the minimum path to the next node
+        for (edge in graph) {
+            if (visited[edge] == false && dist[edge] <= min) {
+                min = dist[edge];
+                index = edge;
+            }
+        }
+
+        // mark as visited
+        visited[index] = true;
+
+        // put the minimum distance in the array
+        for (v in graph) {
+            // check if en edge exists and it's not been visited
+            if (graph[index][v] > 0 && visited[v] == false) {
+                temp = dist[index] + graph[index][v];
+                // check if it's less then what is currently there
+                if(temp < dist[v]) {
+                    dist[v] = temp;
+                }
+            }
+        }
+    }
+
+    return dist;
 }
